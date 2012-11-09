351ee9b8be15b6eaea685787679bfe26c1b9bcfd
Nuwan Sameera <nuwan28@gmail.com>
2012-11-09 07:50:00 +0530

    Change admin tab name

--- app/overrides/add_banner_tab_to_admin.rb
+++ app/overrides/add_banner_tab_to_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "banner_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:banners) %>",
+                     :text => "<%= tab(:widgets) %>",
                      :disabled => false)
