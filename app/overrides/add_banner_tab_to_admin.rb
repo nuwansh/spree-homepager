78e916d2d163cdece95d0b7d79bd948cf8f9ae60
Nuwan Sameera <nuwan28@gmail.com>
2012-11-09 08:23:49 +0530

    Changed the local file

--- app/overrides/add_banner_tab_to_admin.rb
+++ app/overrides/add_banner_tab_to_admin.rb
@@ -1,5 +1,5 @@
 Deface::Override.new(:virtual_path => "spree/layouts/admin",
                      :name => "banner_admin_tab",
                      :insert_bottom => "[data-hook='admin_tabs']",
-                     :text => "<%= tab(:widgets) %>",
+                     :text => "<%= tab(:banners) %>",
                      :disabled => false)
